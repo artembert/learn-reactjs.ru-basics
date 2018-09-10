24d97cfb5b008822224f954572666c0521eec6dd
Artem Beresnev <a.beresnev@runway-agency.ru>
2018-09-10 19:22:42 +0300

    done https://learn-reactjs.ru/basics/conditional-rendering

--- src/App.js
+++ src/App.js
@@ -1,21 +1,39 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+  constructor(props) {
+  	super(props);
+  	this.state = {
+  		temperature: 0
+	  };
+
+  this.onIncrease = this.onIncrease.bind(this);
+  this.onDecrease = this.onDecrease.bind(this);
+
   }
+
+  onIncrease() {
+  	this.setState(prevState => ({
+		  temperature: prevState.temperature + 1
+	  }))
+  }
+
+  onDecrease() {
+  	this.setState(prevState => ({
+		  temperature: prevState.temperature - 1
+	  }))
+  }
+
+	render() {
+		return (
+			<div className="App">
+				<h2>Текущая температура: {this.state.temperature}</h2>
+				<button onClick={this.onDecrease}>-</button>
+				<button onClick={this.onIncrease}>+</button>
+			</div>
+		);
+	}
 }
 
 export default App;
